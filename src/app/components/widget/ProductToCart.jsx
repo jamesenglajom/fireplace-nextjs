3b19f2e62a6f7780ef182c1898eb5d2bac241994
unknown <jamesenglajom@gmail.com>
2024-12-26 17:16:48 +0800

    added phone number

--- src/app/components/widget/ProductToCart.jsx
+++ src/app/components/widget/ProductToCart.jsx
@@ -112,13 +112,12 @@ const ProductToCart = ({ product, loading }) => {
                             </div>
                         </div>
                         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                            <button
-                                type="button"
-                                onClick={() => setOpen(false)}
+                            <a
+                                href="tel:(888)%20977-9085"
                                 className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                             >
                                 Call
-                            </button>
+                            </a>
                             <button
                                 type="button"
                                 data-autofocus
