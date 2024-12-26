ef4dd30c4ae0b67d82bdd9cd2ec100b9c7365569
unknown <jamesenglajom@gmail.com>
2024-12-26 11:59:28 +0800

    redirect / to /fireplaces

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/fireplaces', // Replace with your target path
+        permanent: true, // Set to true for 301 redirects, false for 302 redirects
+      },
+    ];
+  },
 };
 
 export default nextConfig;
