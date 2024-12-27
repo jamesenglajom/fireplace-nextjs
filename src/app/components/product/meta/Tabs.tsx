286f172c1609d75caf6365ffccb2fb1cbfba375c
unknown <jamesenglajom@gmail.com>
2024-12-27 15:57:56 +0800

    adjusted styling for mobile

--- src/app/components/product/meta/Tabs.tsx
+++ src/app/components/product/meta/Tabs.tsx
@@ -40,13 +40,13 @@ const ProductMetaTabs = ({product}:{product:Product}) => {
             <div className="flex">
                 {
                     tabs.map((v,i)=>
-                    <button onClick={()=> handleTabChange(v.name)} key={`meta-tab-${i}`} className={`px-[15px] py-[7px] rounded-tl-lg rounded-tr-lg ${tab === v.name ? 'bg-pallete-orange text-white font-bold':'bg-stone-100 text-stone-500'}`}>
+                    <button onClick={()=> handleTabChange(v.name)} key={`meta-tab-${i}`} className={`py-[3px] px-[7px] md:py-[7px] md:px-[15px] text-[14px] md:text-[16px] rounded-tl-lg rounded-tr-lg ${tab === v.name ? 'bg-pallete-orange text-white font-bold':'bg-stone-100 text-stone-500'}`}>
                         {v.name}
                     </button>
                     )
                 }
             </div>
-            <div className="border p-[20px]">
+            <div className="border p-[20px] text-[14px] md:text-[16px]">
                 {/* tab contents */}
                 <div dangerouslySetInnerHTML={{__html: tabs.filter(i=> i.name === tab)[0]?.content}}></div>
             </div>
