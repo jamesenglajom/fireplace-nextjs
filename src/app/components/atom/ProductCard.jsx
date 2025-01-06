26d72006714a218d29707b11f3aa9d0165bc74a3
Emman <jamesenglajom@gmail.com>
2025-01-06 18:11:27 +0800

    product card whitespace adjustments

--- src/app/components/atom/ProductCard.jsx
+++ src/app/components/atom/ProductCard.jsx
@@ -24,8 +24,8 @@ const ProductCard = ({ product }) => {
   return (
     <Link
       href={`${BASE_URL}/product/${product.custom_url.url}`}
-      className="flex w-full">
-      <div className="overflow-hidden rounded-md border w-full duration-500 hover:scale-105 hover:shadow-xl pb-[8px] hover:border-stone-700">
+      className="flex w-full h-full bg-white overflow-hidden rounded-md border duration-500  hover:shadow-xl pb-[8px] hover:border-stone-700 group">
+      <div className="">
         <div className="w-full flex items-center justify-center h-[230px] overflow-hidden bg-white relative">
           <img src={thumbnail} alt="" className="object-contain h-full" />
           {/* <div className="absolute top-[10px] right-[10px]">
@@ -38,40 +38,41 @@ const ProductCard = ({ product }) => {
               ONSALE
             </div>
           )}
-          <div className="absolute bottom-0 left-0 bg-pallete-orange text-white text-[12px] py-[5px] md:py-[7px] md:px-[15px] flex items-center w-full justify-center gap-[5px]">
+          <div className="absolute bottom-0 left-0 bg-pallete-orange text-white text-[12px] py-[5px] md:py-[7px] md:px-[15px] flex items-center w-full justify-center gap-[5px] invisible group-hover:visible">
             <div className="flex justify-center">
               <div className="font-semibold text-[0.775rem] inline-block text-center">
                 <Icon
                   icon="mi:shopping-cart-add"
-                  className="text-lg font-thin inline-block"
+                  className="text-lg font-thin inline-block mr-[5px]"
                 />
                 {/* <div className="inline-block"> */}
-                CUSTOMIZE TO PURCHASE
+                {/* CUSTOMIZE TO PURCHASE */}
+                SHOP NOW
                 {/* </div> */}
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-[8px] md:gap-[5px] p-[15px]">
-          <div className="text-xs">
+        <div className="flex flex-col px-[15px] pt-[5px] border-t">
+          {/* bg-red-500 */}
+          <div className="text-xs h-[44px] ">
             STARTING AT{" "}
             <span className="text-pallete-orange font-bold text-lg">
               ${formatPrice(parseFloat(product.price).toFixed(2))}
             </span>
           </div>
-          <div className="my-[7px] text-sm h-[100px]">
+          {/* bg-blue-300 */}
+          <div className="text-sm h-[80px] ">
             <div className="line-clamp-4 font-semibold text-stone-700">
               {product.name}
             </div>
-            <div className="text-stone-400 italic font-semilight">
+            {/* <div className="text-stone-400 italic font-semilight">
               by Brand Name
-            </div>
+            </div> */}
           </div>
-          <div className={`flex flex-col gap-[5px]`}>
-            <div
-              className={`flex items-center gap-[5px]  ${
-                product.reviews_rating_sum > 0 ? "visible" : "invisible"
-              }`}>
+          {/* bg-yellow-300 */}
+          <div className={`flex flex-col`}>
+            <div className={`flex items-center gap-[5px]`}>
               <div>
                 <Rating
                   value={product.reviews_rating_sum}
@@ -81,42 +82,46 @@ const ProductCard = ({ product }) => {
                 ({product.reviews_count}){/* (id:{product.id}) */}
               </div>
             </div>
-            <div className="text-pallete-gray text-[0.65rem] md:text-sm h-[33px]">
+            <div className="text-pallete-gray text-[0.65rem] md:text-sm h-[25px]">
               {product.sku}
             </div>
           </div>
-          <div className="flex flex-col md:flex-row md:items-center justify-between gap-[7px]">
-            {product.is_free_shipping && (
-              <div className="flex items-center font-bold gap-[3px]">
-                <div>
-                  <Icon
-                    icon="lucide:circle-check-big"
-                    className={`${
+          {/* bg-green-300  */}
+          <div className="flex  h-[48px] items-center">
+            <div className="flex flex-col md:flex-row md:items-center justify-between gap-[5px]">
+              {
+                // product.is_free_shipping && (
+                <div className="flex items-center font-bold gap-[3px]">
+                  <div>
+                    <Icon
+                      icon="lucide:circle-check-big"
+                      className={`${
+                        product.is_free_shipping
+                          ? "text-pallete-green"
+                          : "text-pallete-gray"
+                      }`}
+                    />
+                  </div>
+                  <div
+                    className={`text-[0.875rem] relative ${
                       product.is_free_shipping
-                        ? "text-pallete-green"
-                        : "text-pallete-gray"
-                    }`}
-                  />
-                </div>
-                <div
-                  className={`text-[0.875rem] relative ${
-                    product.is_free_shipping
-                      ? "text-black"
-                      : "text-pallete-gray line-through"
-                  }`}>
-                  <span
-                    className={`${
-                      product.is_free_shipping
-                        ? "text-pallete-green"
-                        : "text-pallete-gray"
+                        ? "text-black"
+                        : "text-pallete-gray line-through"
                     }`}>
-                    FREE
-                  </span>{" "}
-                  Shipping
+                    <span
+                      className={`${
+                        product.is_free_shipping
+                          ? "text-pallete-green"
+                          : "text-pallete-gray"
+                      }`}>
+                      FREE
+                    </span>{" "}
+                    Shipping
+                  </div>
                 </div>
-              </div>
-            )}
-            {product?.custom_fields?.quick_ship && (
+                // )
+              }
+              {/* {product?.custom_fields?.quick_ship && ( */}
               <div className="flex items-center font-bold gap-[3px]">
                 <div>
                   <Icon
@@ -126,7 +131,8 @@ const ProductCard = ({ product }) => {
                 </div>
                 <div className="text-[0.875rem]">Quick Ship Available</div>
               </div>
-            )}
+              {/* )} */}
+            </div>
           </div>
         </div>
       </div>
