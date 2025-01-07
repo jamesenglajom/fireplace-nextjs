e6923dda98f8432213bde4b0cf2bc1330cf5b5fc
Emman <jamesenglajom@gmail.com>
2025-01-07 17:39:41 +0800

    added 404

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,6 +4,7 @@ export default function NotFound() {
   return (
     <div className="h-[calc(100vh-300px)] md:h-[calc(100vh-500px)] flex justify-center items-center">
       <div className="text-center">
+        <h1 className="text-5xl font-extrabold text-stone-500">404</h1>
         <h2 className="font-extrabold text-stone-600 text-2xl uppercase">
           Not Found
         </h2>
