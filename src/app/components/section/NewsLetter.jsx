83059046925f2bbb5f1662bf2fe49176c425272a
Emman <jamesenglajom@gmail.com>
2025-01-17 14:00:28 +0800

    newsletter to responsive

--- src/app/components/section/NewsLetter.jsx
+++ src/app/components/section/NewsLetter.jsx
@@ -2,7 +2,7 @@ export default function NewsLetter() {
   return (
     <div className="mt-20 bg-[#f1f1f1] py-[30px]">
       <div className="container mx-auto  gap-[30px] flex flex-col justify-center text-center">
-        <div className="text-stone-800 text-lg">
+        <div className="text-stone-800 text-sm md:text-lg px-[20px]">
           Stay in the Loop! Subscribe to Our Mailing List for Exclusive Sales,
           Blogs, Recipes, Guides and more!
         </div>
@@ -11,9 +11,9 @@ export default function NewsLetter() {
             <input
               type="text"
               placeholder="Enter Email Address"
-              className="p-[15px] rounded-none outline-none focus:border-orange-300 focus:ring-orange-300 bg-[#f1f1f1] ring:2 border-stone-900 border md:w-[500px]"
+              className=" text-sm md:text-base p-[15px] rounded-none outline-none focus:border-orange-300 focus:ring-orange-300 bg-[#f1f1f1] ring:2 border-stone-900 border md:w-[500px]"
             />
-            <button className="py-[15px] px-[30px] text-white bg-stone-900 border border-stone-900">
+            <button className="py-[15px] px-[30px] text-white bg-stone-900 border border-stone-900 text-sm md:text-base">
               Join
             </button>
           </div>
