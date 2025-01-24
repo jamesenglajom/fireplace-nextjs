0eaeabd052aa571ea3908e0bebb529074f4786cf
Emman <jamesenglajom@gmail.com>
2025-01-24 14:12:28 +0800

    resize font and remove signin-sign-up

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -33,15 +33,15 @@ export default function RootLayout({ children }) {
         <div className="hidden md:block bg-pallete-orange py-[8px] px-[30px] text-white">
           <div className="container mx-auto  flex items-center justify-between">
             <div className="flex justify-between w-[80%]">
-              <div className="font-extralight">Promotions</div>
-              <div className="font-extralight">Learning Center</div>
-              <div className="font-extralight">Professional Program</div>
-              <div className="font-extralight">Order Status</div>
-              <div className="font-extralight">
+              <div className="font-light text-sm">Promotions</div>
+              <div className="font-light text-sm">Learning Center</div>
+              <div className="font-light text-sm">Professional Program</div>
+              <div className="font-light text-sm">Order Status</div>
+              <div className="font-light text-sm">
                 Why Buy From The Expert In Fire?
               </div>
             </div>
-            <div className="w-[20%] text-right">SignIn | SignUp</div>
+            {/* <div className="w-[20%] text-right">SignIn | SignUp</div> */}
           </div>
         </div>
         <TuiNavBar />
