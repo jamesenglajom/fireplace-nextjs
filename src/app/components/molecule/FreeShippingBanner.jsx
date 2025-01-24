9ba520ea4ccec74d25d31dbd6c3f89ff5a706573
Emman <jamesenglajom@gmail.com>
2025-01-24 14:14:22 +0800

    update display text

--- src/app/components/molecule/FreeShippingBanner.jsx
+++ src/app/components/molecule/FreeShippingBanner.jsx
@@ -3,7 +3,7 @@ export default function FreeShippingBanner() {
   return (
     <div className="bg-[#4C4C53] flex items-center justify-center py-[8px]">
       <div className="text-white mr-[10px] text-[16px]">
-        Free shipping on orders over $99
+        Enjoy free shipping on all orders over $99!
       </div>
       <DeliveryTruckSpeedIcon width="28" height="28" color="white" />
     </div>
