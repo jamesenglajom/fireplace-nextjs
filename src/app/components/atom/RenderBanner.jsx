1630f5b1aa3074cb12c0112767e6e9c3bf7bd6f9
Emman <jamesenglajom@gmail.com>
2025-01-29 16:38:17 +0800

    removed useQuality

--- src/app/components/atom/RenderBanner.jsx
+++ src/app/components/atom/RenderBanner.jsx
@@ -6,12 +6,7 @@ export default function RenderBanner({ img }) {
     { minWidth: 0, value: 50 },
     { minWidth: 640, value: 980 },
   ];
-  const qualityBreakPoints = [
-    { minWidth: 0, value: 90 },
-    { minWidth: 640, value: 100 },
-  ];
   const useWidth = useBreakpointValue(breakPoints);
-  const useQuality = useBreakpointValue(qualityBreakPoints);
   return (
     <Image
       src={img}
@@ -19,7 +14,6 @@ export default function RenderBanner({ img }) {
       className="w-full h-full object-cover"
       width={useWidth ?? 1200}
       height={0}
-      quality={useQuality ?? 90}
       loading="eager"
       priority={true}
       sizes="(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 80vw, 1200px"
