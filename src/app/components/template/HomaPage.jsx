3310584197b8105fea787c068e953d8f7e73b54d
Emman <jamesenglajom@gmail.com>
2025-01-30 13:21:09 +0800

    sections to dynamic except hero and featured category

--- src/app/components/template/HomaPage.jsx
+++ src/app/components/template/HomaPage.jsx
@@ -3,13 +3,13 @@ import dynamic from "next/dynamic";
 import MobileLoader from "@/app/components/molecule/MobileLoader";
 // home page section import in order
 import TuiHero from "@/app/components/template/tui_hero";
-// import FeatureCategoriesSection from "@/app/components/section/HomePageFeatureCategories";
-const FeatureCategoriesSection = dynamic(
-  () => import("@/app/components/section/HomePageFeatureCategories"),
-  {
-    ssr: false,
-  }
-);
+import FeatureCategoriesSection from "@/app/components/section/HomePageFeatureCategories";
+// const FeatureCategoriesSection = dynamic(
+//   () => import("@/app/components/section/HomePageFeatureCategories"),
+//   {
+//     ssr: false,
+//   }
+// );
 // import ShopAllClearanceSection from "@/app/components/section/HomePageShopAllClearance";
 const ShopAllClearanceSection = dynamic(
   () => import("@/app/components/section/HomePageShopAllClearance"),
@@ -31,8 +31,20 @@ const ReviewsSection = dynamic(
     ssr: false,
   }
 );
-import ShopCategorySection from "@/app/components/section/HomePageShopCategory";
-import GuidesAndInspirationSection from "@/app/components/section/HomePageGuidesAndInspiration";
+// import ShopCategorySection from "@/app/components/section/HomePageShopCategory";
+const ShopCategorySection = dynamic(
+  () => import("@/app/components/section/HomePageShopCategory"),
+  {
+    ssr: false,
+  }
+);
+// import GuidesAndInspirationSection from "@/app/components/section/HomePageGuidesAndInspiration";
+const GuidesAndInspirationSection = dynamic(
+  () => import("@/app/components/section/HomePageGuidesAndInspiration"),
+  {
+    ssr: false,
+  }
+);
 // import ShopOpenBoxSection from "@/app/components/section/HomePageShopOpenBox";
 const ShopOpenBoxSection = dynamic(
   () => import("@/app/components/section/HomePageShopOpenBox"),
@@ -40,8 +52,20 @@ const ShopOpenBoxSection = dynamic(
     ssr: false,
   }
 );
-import PartsAndAccessoriesSection from "@/app/components/section/HomePagePartsAndAccessories";
-import FrequentlyAskedSection from "@/app/components/section/HomePageFrequentlyAsked";
+// import PartsAndAccessoriesSection from "@/app/components/section/HomePagePartsAndAccessories";
+const PartsAndAccessoriesSection = dynamic(
+  () => import("@/app/components/section/HomePagePartsAndAccessories"),
+  {
+    ssr: false,
+  }
+);
+// import FrequentlyAskedSection from "@/app/components/section/HomePageFrequentlyAsked";
+const FrequentlyAskedSection = dynamic(
+  () => import("@/app/components/section/HomePageFrequentlyAsked"),
+  {
+    ssr: false,
+  }
+);
 import NewsLetterSection from "@/app/components/section/NewsLetter";
 
 export default function HomePageWrapper({ data }) {
