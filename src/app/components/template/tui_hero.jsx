6d5be26904722c04db88b2365363a210b9fe3d15
Emman <jamesenglajom@gmail.com>
2025-02-04 15:43:36 +0800

    add sub banner

--- src/app/components/template/tui_hero.jsx
+++ src/app/components/template/tui_hero.jsx
@@ -39,8 +39,8 @@ export default function Hero({ data }) {
               </div>
             </div>
           </div>
-          <div className="flex-col flex md:flex-row">
-            <div className="w-full md:w-[calc(100%-250px)] p-[20px]">
+          <div className="flex-col flex lg:flex-row">
+            <div className="w-full lg:w-[calc(100%-250px)] p-[20px]">
               <div className="flex flex-col gap-[20px]">
                 <div className="border-orange-500 border-l-4 text-2xl pl-3 font-bold text-stone-600">
                   {data?.name}
@@ -62,22 +62,32 @@ export default function Hero({ data }) {
                       Further Questions? ASK AN EXPERT!
                     </div>
                     <Link
-                      href={`tel:(888)%20977-9085`}
-                      className="flex md:ml-[8px] text-orange-500 font-bold">
+                      href={`tel:(888)%20575-9720`}
+                      className="flex md:ml-[8px] text-orange-500 font-bold"
+                    >
                       <Icon
                         icon="ic:baseline-phone"
                         className=" mr-[3px]"
                         width="15"
                         height="15"
                       />
-                      (888) 977-9085
+                      (888) 575-9720
                     </Link>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="w-full md:w-[250px] p-[30px] bg-[rgb(151,151,151)] flex flex-col gap-[20px]">
-              <div>
+            <div className="w-full p-[30px] bg-stone-100 flex flex-col gap-[20px] relative lg:w-[250px] h-[360px]">
+              <Image
+                src="/images/banner/sub-banner-image.webp"
+                alt={`Sub Banner Image`}
+                className="object-contain"
+                layout="fill"
+                objectFit="contain"
+                objectPosition="center"
+                sizes="100vw"
+              />
+              {/* <div>
                 <div className="text-white text-3xl font-bold">LEARNING</div>
                 <div className="text-white text-3xl">CENTER</div>
               </div>
@@ -96,7 +106,7 @@ export default function Hero({ data }) {
                   width="20"
                   height="20"
                 />
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
@@ -115,7 +125,8 @@ export default function Hero({ data }) {
                 data.children.map((i, index) => (
                   <div
                     key={`sub-category-${i.name.toLowerCase()}-${index}`}
-                    className="flex">
+                    className="flex"
+                  >
                     <div className="w-[40px] flex justify-center items-center">
                       <Icon
                         icon="material-symbols:info-outline"
@@ -141,7 +152,8 @@ export default function Hero({ data }) {
                             />
                           </div>
                           <div
-                            className={`text-sm uppercase text-stone-600 font-bold w-[calc(100%-95px)] group-hover:text-orange-500`}>
+                            className={`text-sm uppercase text-stone-600 font-bold w-[calc(100%-95px)] group-hover:text-orange-500`}
+                          >
                             {i.name}
                           </div>
                           <div className="text-stone-600 group-hover:text-orange-500">
