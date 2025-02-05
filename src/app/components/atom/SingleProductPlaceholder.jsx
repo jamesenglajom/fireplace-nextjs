c6a6abbf5572d692566373a97b6ce6c52ce18e62
Emman <jamesenglajom@gmail.com>
2025-02-05 15:01:34 +0800

    placeholder update

--- src/app/components/atom/SingleProductPlaceholder.jsx
+++ src/app/components/atom/SingleProductPlaceholder.jsx
@@ -1,47 +1,51 @@
-import React from 'react'
-
+import React from "react";
 
 const loader_thumbnails = [1, 2, 3, 4];
-function SingleProductPlaceholder() {    
+function SingleProductPlaceholder() {
   return (
-    <div className="container mx-auto flex flex-col sm:flex-row gap-[10px] py-[20px]">
+    <div className="container mx-auto">
+      <div className="px-4 mt-2">
+        <div className="rounded-full w-[75px] h-[24px] bg-stone-200"></div>
+      </div>
+      <div className="flex flex-col sm:flex-row gap-[10px] py-[20px]">
         <div className="flex-1">
-            <div className="w-full px-4 mb-8">
-                <div className="flex aspect-w-16 h-[350px] md:aspect-w-4 md:aspect-h-3 overflow-hidden rounded-lg shadow-md mb-4 justify-center items-center bg-stone-200"></div>
-                <div className="flex gap-4 py-4 justify-center overflow-x-auto">
-                    {loader_thumbnails.map((item, index) => (
-                    <div
-                        key={`img-gallery-loader-${index}`}
-                        className={`size-16 sm:size-20 object-cover rounded-md cursor-pointer bg-stone-200 transition duration-300`}
-                    />
-                    ))}
-                </div>
+          <div className="w-full px-4 mb-8">
+            <div className="flex aspect-w-16 h-[350px] md:aspect-w-4 md:aspect-h-3 overflow-hidden rounded-lg shadow-md mb-4 justify-center items-center bg-stone-200"></div>
+            <div className="flex gap-4 py-4 justify-center overflow-x-auto">
+              {loader_thumbnails.map((item, index) => (
+                <div
+                  key={`img-gallery-loader-${index}`}
+                  className={`size-16 sm:size-20 object-cover rounded-md cursor-pointer bg-stone-200 transition duration-300`}
+                />
+              ))}
             </div>
-        </div>
-        <div className="flex-1">
-      <div className="flex flex-col gap-[15px] w-full">
-        <div className="flex gap-[10px]">
-          <div className="py-[5px] px-[25px] bg-stone-200 text-stone-600 h-[34px] w-[170px] font-semibold rounded-full"></div>
-          <div className="py-[5px] px-[25px] bg-stone-200 text-stone-600 h-[34px] w-[155px] font-semibold rounded-full"></div>
-        </div>
-        <div className="">
-          <div className="font-bold text-4xl flex flex-col gap-[2px]">
-            <div className="h-[50px] w-full bg-stone-200"></div>
-            <div className="h-[50px] w-[85%] bg-stone-200"></div>
-            <div className="h-[50px] w-[60%] bg-stone-200"></div>
-            <div className="h-[24px] w-[50%] bg-stone-200"></div>
           </div>
         </div>
-        <div className="mt-[100px]">
-          <div className="font-bold text-4xl flex gap-[20px]">
-            <div className="h-[54px] w-[270px] bg-stone-200 rounded-full"></div>
-            <div className="h-[54px] w-[54px] bg-stone-200 rounded-full"></div>
+        <div className="flex-1">
+          <div className="flex flex-col gap-[15px] w-full">
+            <div className="flex gap-[10px]">
+              <div className="py-[5px] px-[25px] bg-stone-200 text-stone-600 h-[34px] w-[170px] font-semibold rounded-full"></div>
+              <div className="py-[5px] px-[25px] bg-stone-200 text-stone-600 h-[34px] w-[155px] font-semibold rounded-full"></div>
+            </div>
+            <div className="">
+              <div className="font-bold text-4xl flex flex-col gap-[2px]">
+                <div className="h-[50px] w-full bg-stone-200"></div>
+                <div className="h-[50px] w-[85%] bg-stone-200"></div>
+                <div className="h-[50px] w-[60%] bg-stone-200"></div>
+                <div className="h-[24px] w-[50%] bg-stone-200"></div>
+              </div>
+            </div>
+            <div className="mt-[100px]">
+              <div className="font-bold text-4xl flex gap-[20px]">
+                <div className="h-[54px] w-[270px] bg-stone-200 rounded-full"></div>
+                <div className="h-[54px] w-[54px] bg-stone-200 rounded-full"></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-        </div>
     </div>
-  )
+  );
 }
 
-export default SingleProductPlaceholder
+export default SingleProductPlaceholder;
--- src/app/components/pages/product/section/product.jsx
+++ src/app/components/pages/product/section/product.jsx
@@ -48,59 +48,62 @@ const ProductSection = ({ product, loading }) => {
 
   return (
     <>
-        <div className="p-4">
-          <div className="container mx-auto flex flex-col gap-[10px]">
-            <div>
-              <BackButton />
-            </div>
+      <div className="p-4">
+        <div className="container mx-auto flex flex-col gap-[10px]">
+          <div>
+            <BackButton />
           </div>
-          <div className="container mx-auto flex flex-col sm:flex-row gap-[10px] py-[20px]">
-            <div className="flex-1">
-              <MediaGallery mediaItems={mediaItems} loading={loading} />
-            </div>
-            <div className="flex-1">
-              <ProductToCart product={product} loading={loading} />
-              {/* product options */}
-              <div className="py-[30px] flex flex-col gap-[15px]">
-                {productOptions &&
-                  productOptions.length > 0 &&
-                  productOptions.map((item, idx) => (
-                    <div
-                      key={`product-option-${idx}`}
-                      className="flex flex-col gap-[10px]"
-                    >
-                      <div className="font-medium text-xs lg:text-sm">
-                        {item.option}
-                      </div>
-                      <div className="flex items-center gap-[10px]">
-                        {item?.values &&
-                          item.values.map((item2, idx2) => (
-                            <Link
-                              key={`product-option-${idx}-value-${idx2}`}
-                              href={`${BASE_URL}/product/${item2.sku.link}`}
-                            >
-                              <ProductOption option={item2} />
-                            </Link>
-                          ))}
-                      </div>
+        </div>
+        <div className="container mx-auto flex flex-col sm:flex-row gap-[10px] py-[20px]">
+          <div className="flex-1">
+            <MediaGallery mediaItems={mediaItems} loading={loading} />
+          </div>
+          <div className="flex-1">
+            <ProductToCart product={product} loading={loading} />
+            {/* product options */}
+            <div className="py-[30px] flex flex-col gap-[15px]">
+              <div className="font-bold text-sm lg:text-lg mt-1 md:mt-3">
+                Options
+              </div>
+              {productOptions &&
+                productOptions.length > 0 &&
+                productOptions.map((item, idx) => (
+                  <div
+                    key={`product-option-${idx}`}
+                    className="flex flex-col gap-[10px]"
+                  >
+                    <div className="font-medium text-xs lg:text-sm">
+                      {item.option}
                     </div>
-                  ))}
+                    <div className="flex items-center gap-[10px]">
+                      {item?.values &&
+                        item.values.map((item2, idx2) => (
+                          <Link
+                            key={`product-option-${idx}-value-${idx2}`}
+                            href={`${BASE_URL}/product/${item2.sku.link}`}
+                          >
+                            <ProductOption option={item2} />
+                          </Link>
+                        ))}
+                    </div>
+                  </div>
+                ))}
+            </div>
+            <div>
+              <div className="font-bold text-sm lg:text-lg mb-[15px]">
+                Category
               </div>
-              <div>
-                <div className="font-bold text-sm lg:text-lg mb-[15px]">
-                  Category
-                </div>
-                <CategoryChips categories={product?.categories} />
-              </div>
+              <CategoryChips categories={product?.categories} />
             </div>
           </div>
         </div>
-        <div className="p-4">
-          <div className="container mx-auto">
-            <ProductMetaTabs product={product} />
-          </div>
+      </div>
+      <div className="p-4">
+        <div className="container mx-auto">
+          <ProductMetaTabs product={product} />
         </div>
-        {/* <div className="p-4">
+      </div>
+      {/* <div className="p-4">
       <div className="container mx-auto">
         <RelatedProducts product={product}/>
       </div>
