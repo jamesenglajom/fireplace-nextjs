618cd95a4dcb52aeaa3cb2bcf894d4f21b036e8c
Emman <jamesenglajom@gmail.com>
2025-02-24 11:42:00 +0800

    update height for not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="h-[calc(100vh-300px)] md:h-[calc(100vh-500px)] flex justify-center items-center">
+    <div className="h-[calc(100vh-210px)] flex justify-center items-center">
       <div className="text-center">
         <h1 className="text-5xl font-extrabold text-stone-500">404</h1>
         <h2 className="font-extrabold text-stone-600 text-2xl uppercase">
