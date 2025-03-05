6d190243579709a60f2873399922a39b358a180f
jamesenglajom@gmail.com <kayenglajom@gmail.com>
2025-03-05 14:05:54 +0800

    test menu content updates on live

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,17 @@ module.exports = {
   images: {
     domains: ['cdn11.bigcommerce.com', 'onsite-cdn.sfo3.cdn.digitaloceanspaces.com'],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)', // Match all routes
-        headers: [
-          // {
-          //   key: 'Cache-Control',
-          //   value: 'public, max-age=31536000, immutable', // Cache static assets
-          // },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)', // Match all routes
+  //       headers: [
+  //         {
+  //           key: 'Cache-Control',
+  //           value: 'public, max-age=31536000, immutable', // Cache static assets
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
