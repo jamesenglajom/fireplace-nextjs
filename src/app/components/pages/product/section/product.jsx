171e9273f31f69475acb69ed6cf4bfd29c605305
jamesenglajom <jamesenglajom@gmail.com>
2025-03-19 12:35:13 +0800

    added see more toggle on FAQs section for single product page

--- src/app/components/pages/product/section/product.jsx
+++ src/app/components/pages/product/section/product.jsx
@@ -8,16 +8,16 @@ import ProductOption from "@/app/components/atom/productOption";
 import CategoryChips from "@/app/components/atom/SingleProductCategoryChips";
 import YouMayAlsoLike from "@/app/components/molecule/YouMayAlsoLike";
 import BreadCrumbs from "@/app/components/atom/SingleProductBreadCrumbs";
-import ReactHtmlParser from "react-html-parser";
+import parse from "html-react-parser";
 import { useState, useEffect } from "react";
 import { keys, redisGet } from "@/app/lib/redis";
+import { MingcuteUpLine, MingcuteDownLine } from "@/app/components/icons/lib";
 const BASE_URL = process.env.NEXT_PUBLIC_SITE_BASE_URL;
 
 const about = keys.faqs_about_solana.value;
 const shipping_policy = keys.faqs_shipping_policy.value;
 const return_policy = keys.faqs_return_policy.value;
 const warranty = keys.faqs_warranty.value;
-
 const ProductSection = ({ product, loading }) => {
   const [mediaItems, setMediaItems] = useState([]);
   const [productOptions, setProductOptions] = useState([]);
@@ -25,11 +25,35 @@ const ProductSection = ({ product, loading }) => {
   const { productMetaFields, loading: metaFieldsLoading } =
     useFetchProductMetaFields(metafieldParam);
   const [policy_section, setPolicySection] = useState([
-    {key: about, label: "About Solana Fireplaces", content: ""},
-    {key: shipping_policy, label: "Shipping Policy", content: ""},
-    {key: return_policy, label: "Return Policy", content: ""},
-    {key: warranty, label: "Warranty", content: ""},
-  ])
+    {
+      key: about,
+      label: "About Solana Fireplaces",
+      content: "",
+      expanded: false,
+    },
+    {
+      key: shipping_policy,
+      label: "Shipping Policy",
+      content: "",
+      expanded: false,
+    },
+    {
+      key: return_policy,
+      label: "Return Policy",
+      content: "",
+      expanded: false,
+    },
+    { key: warranty, label: "Warranty", content: "", expanded: false },
+  ]);
+
+  const handleExpandFAQsSection = (key) => {
+    setPolicySection((prev) => {
+      return prev.map((i) => ({
+        ...i,
+        expanded: i.key === key ? !i.expanded : i.expanded,
+      }));
+    });
+  };
 
   // console.log("product_id", product.id);
   useEffect(() => {
@@ -96,18 +120,18 @@ const ProductSection = ({ product, loading }) => {
     }
   }, [productMetaFields]);
 
-  useEffect(()=>{
+  useEffect(() => {
     redisGet([about, shipping_policy, return_policy, warranty])
-    .then(response=>{
-      console.log("redisGetResponse", response)
-      setPolicySection(prev=>{
-        return prev.map((item,index)=>{
-          return {...item, content: response[index]}
-        })
+      .then((response) => {
+        console.log("redisGetResponse", response);
+        setPolicySection((prev) => {
+          return prev.map((item, index) => {
+            return { ...item, content: response[index] };
+          });
+        });
       })
-    })
-    .catch(err=> console.log("error", err))
-  },[])
+      .catch((err) => console.log("error", err));
+  }, []);
 
   return (
     <>
@@ -177,13 +201,32 @@ const ProductSection = ({ product, loading }) => {
           <div className="text-2xl font-semibold text-stone-900 my-[20px]">
             FAQS
           </div>
-          <div className="bg-zinc-100 p-5 flex flex-col gap-[50px] rounded">
+          <div className="bg-zinc-100 p-5 flex flex-col gap-[10px] rounded">
             {policy_section.map((i) => (
               <div key={`section-${i.key}`} className="">
-                <div className="text-lg font-semibold text-stone-900 border-b pb-[10px]">
+                <div className="text-lg font-semibold text-stone-900 border-b">
                   {i.label}
                 </div>
-                <div className="text-sm flex flex-col gap-[10px] py-[10px]">{ReactHtmlParser(i.content)}</div>
+                {i.content && (
+                  <div className="relative">
+                    <div
+                      className={`text-sm flex flex-col gap-[10px] py-[10px] ${ i.key !== warranty ? i.expanded ? "" : "!line-clamp-2": "" }`}
+                    >
+                      {parse(i.content)}
+                    </div>
+                    <div className="bg-zinc-100 w-full h-3 absolute left-0 bottom-0"></div>
+                  </div>
+                )}
+                {
+                  i.key !== warranty && 
+                  <button
+                    className="text-sm font-semibold text-blue-400 flex items-center"
+                    onClick={() => handleExpandFAQsSection(i.key)}
+                  >
+                    {i.expanded ? <MingcuteUpLine width={18} height={18}/> : <MingcuteDownLine width={18} height={18}/>}
+                    {i.expanded ? "See Less" : "See More"}
+                  </button>
+                }
               </div>
             ))}
           </div>
