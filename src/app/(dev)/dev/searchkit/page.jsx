6dd4094e876104fe3f4be49a5d39ec7a2e27b641
jamesenglajom <jamesenglajom@gmail.com>
2025-03-21 14:55:38 +0800

    searchkit to /dev/searchkit

---
+++ src/app/(dev)/dev/searchkit/page.jsx
@@ -0,0 +1,12 @@
+'use client'
+import SearchFilter from "@/app/filter/SearchFilter"
+
+function SearchKitPage() {
+  return (
+    <div className="container mx-auto">
+        <SearchFilter />
+    </div>
+  )
+}
+
+export default SearchKitPage
