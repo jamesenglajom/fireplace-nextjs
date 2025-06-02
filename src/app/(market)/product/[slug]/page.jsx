8a367886b14309c65892a5fae77fcd97dab437cf
jamesenglajom <jamesenglajom@gmail.com>
2025-06-02 16:57:49 +0800

    single product page --- product options

--- src/app/(market)/product/[slug]/page.jsx
+++ src/app/(market)/product/[slug]/page.jsx
@@ -8,10 +8,11 @@ import { notFound } from "next/navigation";
 import ProductPlaceholder from "@/app/components/atom/SingleProductPlaceholder";
 
 const shopify_structure = true;
+import Link from "next/link";
 import MediaGallery from "@/app/components/widget/MediaGalleryV2";
 import ProductToCart from "@/app/components/widget/ProductToCartV2";
 import ProductMetaTabs from "@/app/components/product/meta/Tabs";
-
+import { createSlug, formatPrice } from "@/app/lib/helpers";
 
 export default function Product({ params }) {
   const { slug } = React.use(params);
@@ -67,6 +68,127 @@ export default function Product({ params }) {
     );
   };
 
+
+  const ProductOptions = ({product}) => {
+    if(!product && !product?.accentuate_data){
+      return;
+    }
+
+    return (<div className="flex flex-col gap-[20px]">
+      {/* Gas type */}
+      {
+        product.accentuate_data?.[0]?.["bbq.option_related_product"] && 
+       <ProductOptionItem
+       title={product.accentuate_data?.[0]?.["bbq.option_title"]}
+       options={product.accentuate_data?.[0]?.["bbq.option_type"]}
+       urls={product.accentuate_data?.[0]?.["bbq.option_related_product"]}
+       current_url={product.handle}
+       product_options={product.sp_product_options}
+       />
+      }
+      {/* Configuration */}
+      {
+        product.accentuate_data?.[0]?.["bbq.configuration_product"] && 
+       <ProductOptionItem
+       title={product.accentuate_data?.[0]?.["bbq.configuration_heading_title"]}
+       options={product.accentuate_data?.[0]?.["bbq.configuration_type"]}
+       urls={product.accentuate_data?.[0]?.["bbq.configuration_product"]}
+       current_url={product.handle}
+       product_options={product.sp_product_options}
+       />
+      }
+      {/* Product Size */}
+      {
+        product.accentuate_data?.[0]?.["bbq.related_product"] && 
+       <ProductOptionItem
+       title={product.accentuate_data?.[0]?.["bbq.size_heading_title"]}
+       options={product.accentuate_data?.[0]?.["size_title"]}
+       urls={product.accentuate_data?.[0]?.["bbq.related_product"]}
+       current_url={product.handle}
+       product_options={product.sp_product_options}
+       />
+      }
+      {/* Product Option */}
+      {
+        product.accentuate_data?.[0]?.["bbq.product_option_related_product"] && 
+       <ProductOptionItem
+       title={product.accentuate_data?.[0]?.["bbq.product_option_heading_title"]}
+       options={product.accentuate_data?.[0]?.["bbq.product_option_name"]}
+       urls={product.accentuate_data?.[0]?.["bbq.product_option_related_product"]}
+       current_url={product.handle}
+       product_options={product.sp_product_options}
+       />
+      }
+      {/* Hinge */}
+      {
+        product.accentuate_data?.[0]?.["bbq.hinge_related_product"] && 
+       <ProductOptionItem
+       title={product.accentuate_data?.[0]?.["hinge_heading_title"]}
+       options={product.accentuate_data?.[0]?.["hinge_selection"]}
+       urls={product.accentuate_data?.[0]?.["bbq.hinge_related_product"]}
+       current_url={product.handle}
+       product_options={product.sp_product_options}
+       />
+      }
+    </div>)
+  }
+
+  const ProductOptionItem = ({title, options, urls, current_url, product_options}) => {
+    const [localTitle, setLocalTitle] = useState(null);
+    const [localOptions, setLocalOptions] = useState(null);
+    const [localUrls, setLocalUrls] = useState(null);
+    const [localCurrentUrl, setLocalCurrentUrl] = useState(null);
+    const [localProductOptions, setLocalProductOptions] = useState(null);
+    useEffect(()=>{
+      if(title){
+        setLocalTitle(title);
+      }
+      if(options){
+        setLocalOptions(JSON.parse(options));
+      }
+      if(urls){
+        setLocalUrls(JSON.parse(urls));
+      }
+      if(current_url){
+        setLocalCurrentUrl(current_url);
+      }
+      if(product_options){
+        setLocalProductOptions(product_options);
+      }
+    },[title, options, urls, current_url])
+
+    const UpsellPriceDisplay = ({product_price, other_product_price}) => {
+      if(product_price > other_product_price){
+        return <div className="text-green-500 font-semibold">{`Save $${formatPrice(product_price - other_product_price)}`}</div>;
+      }
+      if(product_price < other_product_price){
+        return <div className="text-red-500 font-semibold">{`Add $${formatPrice(other_product_price - product_price)}`}</div>;
+      }
+      if(product_price === other_product_price){
+        return ``;
+      }
+    }
+
+    return (<div>
+      <div className="font-medium text-sm mb-[10px]">{localTitle}</div>
+      <div className="flex flex-wrap gap-[5px]">{
+        localOptions && Array.isArray(localOptions) &&
+        localOptions.map((item,index) => 
+        <Link prefetch={false} href={`/product/${localUrls[index]}`} key={`${createSlug(title)}-option-${index}`} className={`px-3 py-1 rounded border flex flex-col items-center justify-center transition-colors duration-200 ${localUrls[index] === localCurrentUrl ? "font-semibold text-theme-800 border-theme-500 bg-theme-50":"bg-white border-neutral-300 hover:border-neutral-400 hover:bg-neutral-50"}`}>
+          <div className="w-full flex justify-center">{item}</div>
+          <div className="w-full flex justify-end text-xs">{localProductOptions && Array.isArray(localProductOptions) && <>
+          {
+            <UpsellPriceDisplay 
+            product_price={localProductOptions.find(({handle})=> handle === localCurrentUrl)?.variants?.[0]?.price}
+            other_product_price={localProductOptions.find(({handle})=> handle === localUrls[index])?.variants?.[0]?.price}
+            />
+          }
+          </>}</div>
+        </Link>)
+      }</div>
+    </div>)
+  }
+
   return (
     <div className="min-h-screen">
       {shopify_structure ? (
@@ -90,35 +212,35 @@ export default function Product({ params }) {
                 <ProductToCart product={product} loading={loading} />
                 <div className="py-[30px] flex flex-col gap-[15px]">
                   {/* product options */}
-                  {/* commented code because property metafield was changed */}
+                  <ProductOptions product={product} />
                   {/* {productOptions && productOptions.length > 0 && (
-                <div className="flex flex-col gap-[15px]">
-                  <div className="font-bold text-sm lg:text-lg">Options</div>
-                  <div className="border">
-                    {productOptions.map((item, idx) => (
-                      <div
-                        key={`product-option-${idx}`}
-                        className="flex flex-col gap-[10px]"
-                      >
-                        <div className="font-medium text-xs lg:text-sm px-4 py-1 bg-stone-300 text-black">
-                          {item.option}
-                        </div>
-                        <div className="flex items-center gap-[10px] p-1 pb-3">
-                          {item?.values &&
-                            item.values.map((item2, idx2) => (
-                              <Link
-                                key={`product-option-${idx}-value-${idx2}`}
-                                href={`${BASE_URL}/product/${item2.sku.link}`}
-                              >
-                                <ProductOption option={item2} />
-                              </Link>
-                            ))}
-                        </div>
+                    <div className="flex flex-col gap-[15px]">
+                      <div className="font-bold text-sm lg:text-lg">Options</div>
+                      <div className="border">
+                        {productOptions.map((item, idx) => (
+                          <div
+                            key={`product-option-${idx}`}
+                            className="flex flex-col gap-[10px]"
+                          >
+                            <div className="font-medium text-xs lg:text-sm px-4 py-1 bg-stone-300 text-black">
+                              {item.option}
+                            </div>
+                            <div className="flex items-center gap-[10px] p-1 pb-3">
+                              {item?.values &&
+                                item.values.map((item2, idx2) => (
+                                  <Link
+                                    key={`product-option-${idx}-value-${idx2}`}
+                                    href={`${BASE_URL}/product/${item2.sku.link}`}
+                                  >
+                                    <ProductOption option={item2} />
+                                  </Link>
+                                ))}
+                            </div>
+                          </div>
+                        ))}
                       </div>
-                    ))}
-                  </div>
-                </div>
-              )} */}
+                    </div>
+                  )} */}
                   {/* Category */}
                   <div>
                     <div className="font-bold text-sm lg:text-lg mb-[15px]">
--- src/pages/api/es/solana_product.js
+++ src/pages/api/es/solana_product.js
@@ -1,4 +1,3 @@
-
 //  this hook is used for searching products
 export default async function handler(req, res) {
   const ESURL = "http://164.92.65.4:9200";
@@ -28,16 +27,16 @@ export default async function handler(req, res) {
       query: {
         term: {
           "handle.keyword": {
-            value: `${handle}`
+            value: `${handle}`,
           },
         },
       },
     };
 
-
     fetchConfig["body"] = JSON.stringify(new_body);
 
     try {
+      let product_options = null;
       const response = await fetch(API_URL, fetchConfig);
 
       // if (!response.ok) {
@@ -45,7 +44,56 @@ export default async function handler(req, res) {
       // }
       const data = await response.json();
       // elasticsearch result restructured to bigcommerce response object
-      const product = data?.hits?.hits.map((i) => i._source)
+      const product = data?.hits?.hits.map((i) => i._source);
+
+      if (product?.[0] && product[0].accentuate_data?.[0]) {
+        const accentuate_data = product[0].accentuate_data[0];
+        // console.log("accentuate_data",accentuate_data)
+        const keys = [
+          "bbq.related_product",
+          "bbq.configuration_product",
+          "bbq.hinge_related_product",
+          "bbq.option_related_product",
+          "bbq.option_related_product",
+          "bbq.openbox_related_product",
+          "bbq.shopnew_related_product",
+          "bbq.selection_related_product",
+          "frequently.fbi_related_product",
+          "bbq.product_option_related_product",
+          "bbq.configuration_product",
+        ];
+
+        // Flatten all handles from the accentuate_data fields
+        const mergedProducts = mergeRelatedProducts(accentuate_data, keys);
+
+        console.log("Merged Handles:", mergedProducts);
+
+        const secondFetchConfig = {
+          ...fetchConfig,
+          body: JSON.stringify({
+            size: 100,
+            query: {
+              terms: {
+                "handle.keyword": mergedProducts,
+              },
+            },
+          }),
+        };
+
+        const product_options_response = await fetch(
+          API_URL,
+          secondFetchConfig
+        );
+        const product_options_json = await product_options_response.json();
+        product_options = product_options_json?.hits?.hits.map(
+          (i) => i._source
+        );
+      }
+
+      if(product.length > 0){
+        product[0]["sp_product_options"] = product_options;
+      }
+
       const bc_formated_data = {
         data: product,
         requestConfig: fetchConfig,
@@ -57,4 +105,18 @@ export default async function handler(req, res) {
       res.status(500).json({ error: "Failed to fetch products", error });
     }
   }
+}
+
+function mergeRelatedProducts(data, keys) {
+  const merged = [];
+
+  keys.forEach((key) => {
+    // Use lodash-style get or implement your own to handle nested keys like "bbq.related_product"
+    const value = data[key] ? JSON.parse(data[key]) : [];
+    if (Array.isArray(value)) {
+      merged.push(...value);
+    }
+  });
+
+  return merged;
 }
